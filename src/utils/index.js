b630bccdbe78ee79594a27c0ab1985d424fb01a2
Nick Koster <nick.koster@gmail.com>
2022-08-23 21:26:26 +0200

    ignore .env.production

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,11 +9,11 @@ export const movieApi = axios.create({
 
 export const getTmdbToken = async () => {
   try {
-    const { data: { success, request_token } } = await movieApi.get('/authentication/token/new');
+    const { data: { success, request_token: requestToken } } = await movieApi.get('/authentication/token/new');
 
     if (success) {
-      localStorage.setItem('tmdb_token', request_token);
-      window.location.href = `https://www.themoviedb.org/authenticate/${request_token}?redirect_to=${window.location.origin}/approved`;
+      localStorage.setItem('tmdb_token', requestToken);
+      window.location.href = `https://www.themoviedb.org/authenticate/${requestToken}?redirect_to=${window.location.origin}/approved`;
     }
   } catch (error) {
     console.log('Sorry, your token could not be created.', error.message);
@@ -22,18 +22,20 @@ export const getTmdbToken = async () => {
 
 export const createTmdbSessionId = async () => {
   try {
-    const { data: { success, session_id } } = await movieApi.post('/authentication/session/new', {
+    const { data: { success, session_id: sessionId } } = await movieApi.post('/authentication/session/new', {
       request_token: localStorage.getItem('tmdb_token'),
     });
 
-    if (success && session_id !== undefined) {
-      localStorage.setItem('tmdb_session_id', session_id);
+    if (success && sessionId !== undefined) {
+      localStorage.setItem('tmdb_session_id', sessionId);
     }
 
-    return session_id;
+    return sessionId;
   } catch (error) {
     console.log('Sorry, your TMDB session could not be created.', error.message);
   }
+
+  return null;
 };
 
 export const logout = () => {
